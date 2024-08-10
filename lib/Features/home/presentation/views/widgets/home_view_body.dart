792b0d44d1cea99f86d7d1b2c60f53f765467145
MohamedSayed237 <mohamed.sayed23702@gmail.com>
2024-08-10 12:00:42 +0300

    add featured list view to home view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,7 +12,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
     );
   }
