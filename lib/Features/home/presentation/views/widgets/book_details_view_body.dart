e818ab65f358ebb327c1b276ca3643e47973bbef
MohamedSayed237 <mohamed.sayed23702@gmail.com>
2024-08-12 17:09:54 +0300

    add rating to book details view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly_app/core/utils/styles.dart';
@@ -40,7 +41,13 @@ class BookDetailsViewBody extends StatelessWidget {
                   fontWeight: FontWeight.w500,
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            BookRating(
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
           ],
         ),
       ),
